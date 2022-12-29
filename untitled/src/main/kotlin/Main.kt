051827b0001a64ab728572ce437d482dc8d92203
Imperfectt <103348127+Imperfectt@users.noreply.github.com>
2022-12-29 13:59:36 +0500

    Add files via upload

---
+++ untitled/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    println("Таратунина Анастасия, Профессия: Программист") //задание 1
+     var age: Int = 1 //задание 2
+     print("Переменная: $age\n")
+    var name = "hhhh"
+     var ff: Char = 'd'
+     var bb: Float = 2.4f
+     var gg: Double = 1.433223421334433222
+    val number: Int = 4 //нельзя менять значения у val
+    print("$bb\n")
+    print("$ff\n")
+    print("$name\n")
+    print("$gg\n")
+    print("Имя: ")
+    var r = readLine() //задание 3
+    print("Привет, $r")
+
+}
